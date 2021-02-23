b1854a0347bb6e5e522d1c0fec110d13ed453aec
VaalaCat <vaalacat@outlook.com>
2021-02-23 12:54:57 +0000

    新增抽象功能

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 sh install.sh
 
-sed -i '/url/s/url: .*/url: '"$URL"'/g' /var/www/qqbot/config/default.yaml;
-sed -i '/wsurl/s/wsurl: .*/wsurl: '"$WSURL"'/g' /var/www/qqbot/config/default.yaml;
 sed -i '/qqid/s/qqid: .*/qqid: '"$QQID"'/g' /var/www/qqbot/config/default.yaml;
 sed -i '/authKey/s/authKey: .*/authKey: '"$AUTHKEY"'/g' /var/www/qqbot/config/default.yaml;
 sed -i '/adminqqid/s/adminqqid: .*/adminqqid: '"$ADMINQQID"'/g' /var/www/qqbot/config/default.yaml;
--- src/install.sh
+++ src/install.sh
@@ -3,3 +3,9 @@ npm config set registry https://registry.npm.taobao.org
 if [ ! -d "/var/www/qqbot/node_modules" ]; then
   npm install
 fi
+
+if [ -f "/var/www/qqbot/config/default.yaml" ]; then
+  rm "/var/www/qqbot/config/default.yaml"
+fi
+
+cp "/var/www/qqbot/config/default.dev.yaml" "/var/www/qqbot/config/default.yaml"
