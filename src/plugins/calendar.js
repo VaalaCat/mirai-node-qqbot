75bfab6f8d07120d711486dc14371c4b8ff58cea
Cat Vaala <vaalacat@outlook.com>
2021-12-17 18:47:48 +0800

    修改日历实现

--- src/plugins/calendar.js
+++ src/plugins/calendar.js
@@ -1,46 +1,70 @@
 const request = require("request");
+const ical = require('node-ical');
+const mytime = require('./time');
 
 async function func_calendar(mirai, sender, msg, query) {
     return new Promise(function (resolve, reject) {
         let mode = msg.type;
-        if (mode != "GroupMessage") { msg.reply("此功能不支持私聊"); resolve(0); return; }
+        // if (mode != "GroupMessage") { msg.reply("此功能不支持私聊"); resolve(0); return; }
         var options = {
-            "url": "https://api.yoshino-s.online/calendar/recent",
+            "url": "https://api.ctfhub.com/User_API/Event/getAllICS",
             "method": "GET"
         };
         function callback(error, response, body) {
             if (!error && response.statusCode == 200) {
-                body = JSON.parse(body)
-                body = calendar_message_generator(body);
-                msg.reply(body);
-                resolve(0);
-            }
-            else {
-                console.log("Network failed");
-                resolve(1);
+                body = ical.sync.parseICS(body);
+                let eve = []
+                let output = ""
+                // msg.reply(body);
+                // body.url
+                // body.summary
+                // body.start/end
+                // console.log(body);
+
+                for (let k in body) {
+                    if (body.hasOwnProperty(k)) {
+                        const ev = body[k];
+                        if (body[k].type == 'VEVENT') {
+                            let tmpDate = new Date()
+                            if (tmpDate < ev.end) {
+                                eve.push(body[k])
+                            }
+                        }
+                    }
+                }
+                eve.sort((a, b) => { return a.start - b.start })
+                if (query.length == 2) {
+                    if (query[1] == "list") {
+                        output = '以下为近期比赛列表，#calendar 序号 获取详情:\n'
+                        for (i in eve) {
+                            if (i > 10) {
+                                break
+                            }
+                            output += `${i}: ${eve[i].summary}\n`
+                        }
+                    }
+                    else if (parseInt(query[1]) != NaN && parseInt(query[1]) <= 10 && parseInt(query[1]) >= 0) {
+                        let cur = parseInt(query[1])
+                        output += `比赛名称: ${eve[cur].summary}\n`
+                        output += `开始时间: ${eve[cur].start.getFullYear()}/${eve[cur].start.getMonth()}/${eve[cur].start.getDate()}->${eve[cur].start.toLocaleTimeString('en-GB')}\n`
+                        output += `结束时间: ${eve[cur].end.getFullYear()}/${eve[cur].end.getMonth()}/${eve[cur].end.getDate()}->${eve[cur].end.toLocaleTimeString('en-GB')}\n`
+                        output += `链接地址: ${eve[cur].url}\n`
+                        output += `时区: ${eve[cur].vtimezone}\n`
+                        output += `比赛详情: ${eve[cur].description}\n`
+                    } else {
+                        msg.reply("格式错误,请输入 #calendar list 查看比赛列表")
+                    }
+                    msg.reply(output)
+                    resolve(0);
+                }
+                else {
+                    console.log("Network failed");
+                    resolve(1);
+                }
             }
         }
         request(options, callback);
     })
-}
-
-function calendar_message_generator(body) {
-    let ans = "最近的比赛有：";
-    for (i in body) {
-        let tmp = "\n\n";
-        let starttime = body[i]["start"];
-        let endtime = body[i]["end"];
-        starttime = new Date(starttime);
-        endtime = new Date(endtime);
-        starttime = starttime.toLocaleDateString() + " " + starttime.toLocaleTimeString();
-        endtime = endtime.toLocaleDateString() + " " + endtime.toLocaleTimeString();
-        tmp += (body[i]["name"] + ":\n");
-        tmp += ("网址:" + body[i]["website"] + "\n");
-        tmp += ("开始时间:" + starttime + "\n");
-        tmp += ("结束时间:" + endtime);
-        ans += tmp;
-    }
-    return ans;
 }
 
 module.exports = {
