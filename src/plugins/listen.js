3501e5d104710ea7de774ac11fe95671801131d4
VaalaCat <vaalacat@outlook.com>
2022-03-17 10:03:43 +0000

    添加代理支持

--- src/plugins/listen.js
+++ src/plugins/listen.js
@@ -1,22 +1,28 @@
 var message_listener = require("../message_listener");
 const TelegramBot = require('node-telegram-bot-api');
+const config = require("config");
+const conf = config.get("basic");
 const tgtoken = process.env.TGTOKEN || ''
-const bot = new TelegramBot(tgtoken,{polling: true});
-
+const bot = new TelegramBot(tgtoken, {
+	polling: true,
+	request: {
+		proxy: conf.proxy,
+	},
+});
 // listen groupid tgchatid
 async function func_listen(mirai, sender, msg, query) {
 	return new Promise(function (resolve, reject) {
-		if(query[1]=="getid"){
-			bot.on('message', (msg)=>{
+		if (query[1] == "getid") {
+			bot.on('message', (msg) => {
 				bot.sendMessage(msg.chat.id, `消息${msg.text}来自${msg.chat.id}`);
 			})
-		}else if(query[2]!=null){
-			message_listener.listenGroup(query[1],query[2]);
+		} else if (query[2] != null) {
+			message_listener.listenGroup(query[1], query[2]);
 			msg.reply(`群聊${query[1]}的消息将会转发到${query[2]}`);
 		}
 	});
 }
 
-module.exports={
+module.exports = {
 	func_listen
 }
--- src/plugins/tgredir.js
+++ src/plugins/tgredir.js
@@ -1,17 +1,23 @@
 const TelegramBot = require('node-telegram-bot-api');
-
+const config = require("config");
+const conf = config.get("basic");
 const tgtoken = process.env.TGTOKEN || ''
-const bot = new TelegramBot(tgtoken,{polling: false});
+const bot = new TelegramBot(tgtoken, {
+	polling: false,
+	request: {
+		proxy: conf.proxy,
+	},
+});
 
 async function func_tgredir(mirai, sender, msg, query) {
 	return new Promise(function (resolve, reject) {
-		mode=msg.type;
+		mode = msg.type;
 		senderName = (typeof msg.sender.memberName == "undefined" ? msg.sender.nickname : msg.sender.memberName);
 		// if (mode == "TempMessage" || mode == "GroupMessage") {
 		// 	senderName ="来自 "+ msg.sender.group.name+" 的 "+senderName;
 		// }
 		senderName += `(${sender.split(":")[1]})说：\n`
-		bot.sendMessage(query[1],senderName+query[0]);
+		bot.sendMessage(query[1], senderName + query[0]);
 	})
 }
 
